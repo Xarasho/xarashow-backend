4abdaa0ae5c497713a4be5bcd362d098a9fbccd8
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-07-04 12:45:05 -0600

    Added 2 mutations to the SessionResolver to handle the session login and logout

--- src/session/session.resolver.ts
+++ src/session/session.resolver.ts
@@ -1,8 +1,25 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
 
+import { UserModel } from '../modules/auth/account/models/user.model'
+import { GqlContext } from '../shared/types/gql-context.types'
+
+import { LoginInput } from './inputs/login.input'
 import { SessionService } from './session.service'
 
 @Resolver('Session')
 export class SessionResolver {
 	public constructor(private readonly sessionService: SessionService) {}
+
+	@Mutation(() => UserModel, { name: 'login' })
+	public async login(
+		@Context() { req }: GqlContext,
+		@Args('data') input: LoginInput
+	) {
+		return this.sessionService.login(req, input)
+	}
+
+	@Mutation(() => Boolean, { name: 'logout' })
+	public async logout(@Context() { req }: GqlContext) {
+		return this.sessionService.logout(req)
+	}
 }
